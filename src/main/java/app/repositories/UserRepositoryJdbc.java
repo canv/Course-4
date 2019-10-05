db203fd6a869bfea1901b8bd6a009c1e3f7fc17f
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-10-05 21:03:17 +0400

    Java.ASCourse in progress(3) Lesson17

--- src/main/java/app/repositories/UserRepositoryJdbc.java
+++ src/main/java/app/repositories/UserRepositoryJdbc.java
@@ -1,11 +1,16 @@
 package app.repositories;
 
 import app.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Set;
 import java.util.UUID;
 
 public class UserRepositoryJdbc implements UserRepository {
+    //@Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public User findByID(UUID id) {
         return null;
