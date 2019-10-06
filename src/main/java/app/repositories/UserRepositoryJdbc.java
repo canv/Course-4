fd4d1a1bcbebba221ec4a5ba098e15fca1aa4d23
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-10-06 21:48:44 +0400

    Java.ASCourse in progress(4, DB setup) Lesson17

--- src/main/java/app/repositories/UserRepositoryJdbc.java
+++ src/main/java/app/repositories/UserRepositoryJdbc.java
@@ -3,13 +3,24 @@ package app.repositories;
 import app.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
+@Repository
 public class UserRepositoryJdbc implements UserRepository {
-    //@Autowired
+
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        Map<String, Object> stringObjectMap = jdbcTemplate.queryForMap("" +
+                "select id,userName from Users where id='f0425370-d93b-4c03-bc74-925f49cafd67'");
+        System.out.println(stringObjectMap.get("id"));
+    }
 
     @Override
     public User findByID(UUID id) {
